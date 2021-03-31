2273ebcfb591e6b9d9023e749a25d6bf82b08208
Lil Peach <kieefuanh@gmail.com>
2021-03-31 10:27:13 +0700

    base form Dish, Cate, Cus for Khoa

--- DoAnJava/src/Interface/frmDish.java
+++ DoAnJava/src/Interface/frmDish.java
@@ -1,5 +1,4 @@
 /*
-/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -13,12 +12,12 @@ import javax.swing.plaf.basic.BasicInternalFrameUI;
  *
  * @author kieef
  */
-public class frmCate extends javax.swing.JInternalFrame {
+public class frmDish extends javax.swing.JInternalFrame {
 
     /**
      * Creates new form frmStaff
      */
-    public frmCate() {
+    public frmDish() {
         initComponents();
         setTitle("STAFF PAGE");
         
